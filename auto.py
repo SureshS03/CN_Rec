68a829fdf0abf566c4a1eabecf25e7b2949ad990
Suresh S <suthamani51@gmail.com>
2024-10-29 01:08:52 +0530

    Added auto rename

--- auto.py
+++ auto.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+files_name = ['GroupChat.java','Client.java','Server.java','arp_rarp.java','RMIClient.java','RMIServer.java','myinterface.java']
 name = input('Enter your name: ')
 reg = input('Enter your registration number: ')
 # Get the current working directory
@@ -7,19 +8,21 @@ cwd = os.getcwd()
 #print(cwd)
 dris = [d for d in os.listdir(cwd) if os.path.isdir(d)]
 print(dris)
-
+files = []
 for i in dris:
-    #print(i)
-    files = os.listdir(i)
-    #print(files)
-    if files in {'Client.java','Server.java','arp_rarp.java','RMIClient.java','RMIServer.java'}:
-        for f in files:
-            try:
-                with open(f, 'r') as file:
-                    con = file.read()
-                update_con = con.replace('SURESH S', name)
-                update_con = update_con.replace('221211101143', reg)
-                with open(f, 'w') as file:
-                    file.write(update_con)
-            except Exception as e:
-                print(e)
+    x = os.listdir(i)
+    for j in x:
+        if j in files_name:
+            files.append(i+'/'+j)
+for f in files:
+    print(f)
+    try:
+        with open(f, 'r') as file:
+            con = file.read()
+        update_con = con.replace('SURESH S', name)
+        update_con = update_con.replace('221211101143', reg)
+        with open(f, 'w') as file:
+            file.write(update_con)
+            print(f'File {f} updated successfully')
+    except Exception as e:
+        print(e)
